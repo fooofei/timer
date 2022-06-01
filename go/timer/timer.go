dd3b3647510794f7ccef63b66f4983a7a7544f55
fooofei <aihujianfei@qq.com>
2022-06-01 11:15:47 +0800

    add Reset()

--- go/timer/timer.go
+++ go/timer/timer.go
@@ -19,14 +19,16 @@ func New(d time.Duration) *Timer {
 	}
 }
 
+// Wait just returns t.T.C for read
 func (t *Timer) Wait() <-chan time.Time {
 	return t.T.C
 }
 
+// After will restart a new timer with dur
+// returns t.Wait() for read
+// The restart is safely, auto stop the old timer and keep channel clean
 func (t *Timer) After(dur time.Duration) <-chan time.Time {
-	t.Stop()
-	t.T.Reset(dur)
-	t.setActive()
+	t.Reset(dur)
 	return t.Wait()
 }
 
@@ -40,6 +42,8 @@ func (t *Timer) setActive() {
 	atomic.AddInt64(&t.active, 1)
 }
 
+// Stop will stop timer safely than time.Timer Stop()
+// It will read the dirty data from t.T.C for keep t.T.C clean
 func (t *Timer) Stop() bool {
 	success := t.T.Stop()
 	if !success && atomic.LoadInt64(&t.active) > 1 {
@@ -52,6 +56,15 @@ func (t *Timer) Stop() bool {
 		t.SetUnActive()
 	}
 	return success
+}
+
+// Reset will reuse a timer with a new time start
+// old timer will be stop safely
+func (t *Timer) Reset(dur time.Duration) bool {
+	t.Stop()
+	active := t.T.Reset(dur)
+	t.setActive()
+	return active
 }
 
 // Go's timer:
